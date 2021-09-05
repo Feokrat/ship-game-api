423a1ed85a931f52b5e88eb72353713832cd9fdb
Feokrat <dreamking4@gmail.com>
2021-09-05 14:04:20 +0300

    Fix level from string to int.

--- pkg/models/ship.go
+++ pkg/models/ship.go
@@ -5,7 +5,7 @@ import "github.com/google/uuid"
 type Ship struct {
 	Id     uuid.UUID `json:"ship_id" db:"id"`
 	Name   string    `json:"ship_name" db:"name"`
-	Level  string    `json:"ship_level" db:"level"`
+	Level  int       `json:"ship_level" db:"level"`
 	Health float32   `json:"health" db:"health"`
 	Attack float32   `json:"attack" db:"attack"`
 	Luck   float32   `json:"luck" db:"luck"`
